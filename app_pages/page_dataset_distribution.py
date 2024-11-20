bde8e4d4efe12293e35c25319d9932f4335b1987
Profess0ro <professoro88@gmail.com>
2024-11-20 13:44:19 +0100

    Feat: Dataset app page
    Added content to the dataset app page:
    - distribution through all images
    - color differences between all images

--- app_pages/page_dataset_distribution.py
+++ app_pages/page_dataset_distribution.py
@@ -1,13 +1,39 @@
 import streamlit as st
 
+version = 'v2'
+
 def page_dataset_body():
     st.header("Dataset")
-    st.image("../outputs/v2/labels_distribution_raw.png")
-    st.info("""
-            daisy: 5064 images
-            dandelion: 7184 images
-            roses: 5128 images
-            sunflowers: 5592 images
-            tulips: 6392 images
-            """)
-    st.image("../outputs/v2/color_distribution_by_flower_type.png")
+    
+    if st.checkbox(
+        'Display image distribution through all flower species'
+    ):
+        st.image(f"outputs/{version}/labels_distribution_raw.png")
+        st.warning(""" 
+            💐 Distribution of images through all species:  
+            Daisy: 5064 images  
+            Dandelion: 7184 images  
+            Roses: 5128 images  
+            Sunflowers: 5592 images  
+            Tulips: 6392 images
+        """)
+
+    if st.checkbox(
+        'Display the color variation through all images'
+    ):
+        st.image(f"outputs/{version}/color_distribution_by_flower_type.png")
+        st.success("""
+                   This scatterplot visualizes the largest variations in image colors. 
+                   Instead of representing colors using the three RGB channels, PCA reduces 
+                   this to two dimensions (PC1 and PC2), which capture the most significant 
+                   differences in color distribution across all images.
+                   """)
+        st.image(f"outputs/{version}/flower_similarity_heatmap.png")
+        st.success("""
+                   This heatmap helps to visually compare how much the color 
+                   distribution differs between the various flower types in the dataset. 
+                   It shows which flowers are closer in terms of their visual appearance 
+                   (based on their color profiles) and which are more distinct.  
+                   The higher the value, the greater the difference in the color distributions 
+                   between the flowers.
+                   """)
--- app_pages/page_performance_metrics.py
+++ app_pages/page_performance_metrics.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
+version = 'v2'
 def page_performance_body():
     st.header("ML performance metrics")
+    
+    st.image(f"outputs/{version}/color_distribution_by_flower_type.png")
+    st.image(f"outputs/{version}/flower_similarity_heatmap.png")
